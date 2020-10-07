022f7ca72780751a5ac858a862009e393455b542
Justin Macdonald Hansen <just1392@edu.easj.dk>
2020-10-07 17:35:00 +0200

    Sort of finished task 9 and 8, just missing event listeners

--- src/js/index.ts
+++ src/js/index.ts
@@ -4,6 +4,10 @@ import { Cortado } from "./Cortado";
 import { Latte } from "./Latte";
 import {IMilk} from "./IMilk"
 
+let menuCount: number = 0;
+let menuList: HTMLUListElement = <HTMLUListElement> document.getElementById("menuArea");
+let menuButtonCount: number = 0;
+
 interface Person {
     firstName: string;
     lastName: string;
@@ -32,7 +36,61 @@ function PrintCoffeeMenu(coffee: Coffee){
     console.log(coffee.GetCoffeeString());
 }
 
+// function CreateListItem(coffee: Coffee){
+//     let newItem = document.createElement("li");
+//     let newItemText: Text = document.createTextNode(coffee.GetCoffeeString());
+//     let orderButtonText: Text = document.createTextNode("order");
+//     let orderButton: HTMLButtonElement = document.createElement("button");
+    
+//     orderButton.appendChild(orderButtonText);
+//     orderButton.setAttribute("id", "b" + menuButtonCount.toString());
+//     orderButton.setAttribute("class", "orderButton");
+    
+//     newItem.appendChild(newItemText);
+//     newItem.setAttribute("id", menuCount.toString());
+//     newItem.setAttribute("class", "MenuItem");
+//     newItem.appendChild(orderButton);
+    
+//     menuList.appendChild(newItem);
+
+//     menuCount++;
+//     menuButtonCount++;
+// }
+
+function CreateNewRowItem(coffee: Coffee, mlm: number){
+    let table: HTMLTableElement = <HTMLTableElement> document.getElementById("menuTable");
+    let newRow: HTMLTableRowElement = <HTMLTableRowElement> table.insertRow(-1);
+    let name: HTMLTableCellElement = newRow.insertCell(0);
+    let price: HTMLTableCellElement = newRow.insertCell(1);
+    let discount: HTMLTableCellElement = newRow.insertCell(2);
+    let strength: HTMLTableCellElement = newRow.insertCell(3);
+    let mlmilk: HTMLTableCellElement = newRow.insertCell(4);
+    let orderButtonCell: HTMLTableCellElement = newRow.insertCell(5);
+
+    let orderButton: HTMLButtonElement = document.createElement("button");
+    let orderButtonText: Text = document.createTextNode("order");
+    orderButton.appendChild(orderButtonText);
+    orderButton.setAttribute("id", "b" + menuButtonCount.toString());
+    orderButton.setAttribute("class", "orderButton");
+
+    name.innerHTML = coffee.CoffeeDrink();
+    price.innerHTML = coffee.Price().toString();
+    discount.innerHTML = coffee.Discount.toString();
+    strength.innerHTML = coffee.Strength();
+    mlmilk.innerHTML = mlm.toString();
+    orderButtonCell.appendChild(orderButton);
+}
+
 PrintCoffeeMenu(cort);
 PrintCoffeeMenu(latt);
 PrintCoffeeMenu(bc);
 PrintCoffeeMenu(bcNoDiscount);
+
+// CreateListItem(cort);
+// CreateListItem(latt);
+// CreateListItem(bc);
+// CreateListItem(bcNoDiscount);
+CreateNewRowItem(cort, cort.MlMilk());
+CreateNewRowItem(latt, latt.MlMilk());
+CreateNewRowItem(bc, 0);
+CreateNewRowItem(bcNoDiscount, 0);
